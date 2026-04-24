519f7193a384edc5582a3d791790dc1c5fef9292
msantos06-eng <m.santos.06@icloud.com>
2026-04-23 21:08:00 -0300

    Atualizar o login.py

--- frontend/pages/login.py
+++ frontend/pages/login.py
@@ -1,17 +1,10 @@
 import streamlit as st
-from services.api import login
 
-def show_login():
-    st.title("Login")
+st.title("Login")
 
-    email = st.text_input("Email")
-    password = st.text_input("Senha", type="password")
+email = st.text_input("Email")
+senha = st.text_input("Senha", type="password")
 
-    if st.button("Entrar"):
-        res = login(email, password)
-
-        if "access_token" in res:
-            st.session_state["token"] = res["access_token"]
-            st.success("Login OK")
-        else:
-            st.error("Erro no login")
+if st.button("Entrar"):
+    # chamada API aqui
+    st.session_state["token"] = "ok"
