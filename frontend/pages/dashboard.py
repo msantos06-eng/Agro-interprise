10f9cce75945f9a2e2fdf0c513385536a80bd734
msantos06-eng <m.santos.06@icloud.com>
2026-04-23 21:09:12 -0300

    Atualizar o dashboard.py

--- frontend/pages/dashboard.py
+++ frontend/pages/dashboard.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+import streamlit as st
+
+if not st.session_state.get("token"):
+    st.warning("Faça login primeiro")
+    st.stop()
+
+st.title("Dashboard")
+
 def show_dashboard():
     st.title("Dashboard Agro Saas")
     st.write("Bem-vindo ao sistema")
