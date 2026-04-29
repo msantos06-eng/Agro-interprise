cde2ca5bd5debc6613b91c2a932cf93c8cc3444f
msantos06-eng <m.santos.06@icloud.com>
2026-04-29 19:47:11 -0300

    Criar db.py

---
+++ backend/db.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={"connect_timeout": 5}  # 🔥 evita travar
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
