a2e23ea17f7344c2952e0e3c42043dac2714dd33
msantos06-eng <m.santos.06@icloud.com>
2026-04-29 19:57:55 -0300

    Atualizar o db.py

--- backend/db.py
+++ backend/db.py
@@ -2,14 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
-
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Corrige formato do Railway
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
 
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
-    connect_args={"connect_timeout": 5}  # 🔥 evita travar
+    connect_args={"connect_timeout": 5}
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
