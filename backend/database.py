48f1a5d3883d0bc95584d20df9477dc3abe8767a
msantos06-eng <m.santos.06@icloud.com>
2026-05-04 20:48:54 -0300

    Atualizar o database.py

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL não está definida nas variáveis de ambiente")
 
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
